813c4ac83a9ad2c331ffb1b0cfe678219639a6dd
Xiao YiFang <yifang.xiao@gmail.com>
2022-08-06 11:08:13 +0800

    t

--- OcrEngine.h
+++ OcrEngine.h
@@ -26,8 +26,8 @@ along with Capture2Text.  If not, see <http://www.gnu.org/licenses/>.
 #include <QMutex>
 #include <QRect>
 
-#if defined( Q_OS_WIN32 )
-#include "tesseract\baseapi.h"
+#if defined( Q_OS_WIN32 ) || defined( Q_OS_MAC )
+#include "tesseract/baseapi.h"
 #else
 #include "baseapi.h"
 #endif
